2ca6bb95699a1f13d8a9c59e6dac8c5d2659492b
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-20 11:38:38 -0700

    import AppLayout and set up child routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
@@ -15,14 +16,16 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          {/* When it hit /(home) path, it redirect to /dashboard */}
-          <Route index element={<Navigate replace to='dashboard' />} />
-          <Route path='dashboard' element={<Dashboard />} />
-          <Route path='bookings' element={<Bookings />} />
-          <Route path='cabins' element={<Cabins />} />
-          <Route path='users' element={<Users />} />
-          <Route path='settings' element={<Settings />} />
-          <Route path='account' element={<Account />} />
+          <Route element={<AppLayout />}>
+            {/* When it hit /(home) path, it redirect to /dashboard */}
+            <Route index element={<Navigate replace to='dashboard' />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='bookings' element={<Bookings />} />
+            <Route path='cabins' element={<Cabins />} />
+            <Route path='users' element={<Users />} />
+            <Route path='settings' element={<Settings />} />
+            <Route path='account' element={<Account />} />
+          </Route>
           <Route path='login' element={<Login />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
