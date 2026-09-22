b9483520ee515d1e347369a96b556eb07ae56061
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-22 15:57:11 -0700

    set up react query and dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import GlobalStyles from './styles/GlobalStyles';
 import AppLayout from './ui/AppLayout';
 import Dashboard from './pages/Dashboard';
@@ -10,9 +12,20 @@ import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 
+// React Query  QualyClient - basically cache
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // amount of time data stays fresh 60 seconds x 1000 mili seconds = 1min
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +43,7 @@ function App() {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
