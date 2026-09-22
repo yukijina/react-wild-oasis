e3db37647b9040df1fe3b17abddd55614ab3b8d9
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-22 06:04:56 -0700

    create function to fetch cabins data from external api

---
+++ src/services/apiCabins.js
@@ -0,0 +1,13 @@
+import supabase from './supabase';
+
+export async function getCabins() {
+  const { data, error } = await supabase.from('cabins').select('*');
+
+  console.log(data);
+  if (error) {
+    console.error(error);
+    throw new Error('Cabin could not be loaded');
+  }
+
+  return data;
+}
