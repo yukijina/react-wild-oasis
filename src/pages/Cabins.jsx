c35f5cab2023997dd38e2e7ea2115b8a2deac079
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-23 06:26:04 -0700

    remove test data (useEffect), add child component - CabinTbale.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,18 +1,26 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
+// import { getCabins } from '../services/apiCabins';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
+  // test fetch
+  // useEffect(function () {
+  //   getCabins().then((data) => console.log(data));
+  // }, []);
 
   return (
-    <Row type='horizontal'>
-      <Heading as='h1'>All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type='horizontal'>
+        <Heading as='h1'>All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
