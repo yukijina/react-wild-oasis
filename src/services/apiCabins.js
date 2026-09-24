67ec697c8e930117168dba218b547f9581e7fa4d
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-24 15:35:32 -0700

    create delete function

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -3,10 +3,21 @@ import supabase from './supabase';
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
 
-  console.log(data);
+  // console.log(data);
   if (error) {
     console.error(error);
     throw new Error('Cabin could not be loaded');
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from('cabins').delete().eq('id', id);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabin could not be deleted');
   }
 
   return data;
