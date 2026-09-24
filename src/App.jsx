463c2a74aa6982b38edf2cf7acbd74891a28814e
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-24 15:33:41 -0700

    import toaster, setup for success and error message

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Settings from './pages/Settings';
 import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
+import { Toaster } from 'react-hot-toast';
 
 // React Query  QualyClient - basically cache
 const queryClient = new QueryClient({
@@ -44,6 +45,24 @@ function App() {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      {/* / react hot toast */}
+      <Toaster
+        position='top-center'
+        gutter={12}
+        containerStyle={{ magin: '8px' }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24 px',
+            backgroundColor: 'var(--color-grey-0',
+            color: 'var(--color-grey-700',
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
